d4a3e2b6086a7c963e8f5ef978ba07a7f41c95ad
Oscare101 <kirillfedortsev2000@gmail.com>
2023-04-03 09:46:13 +0300

    frinds list add

--- screens/FriendsListScreen.js
+++ screens/FriendsListScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import { FlatList, Text, View, StyleSheet, Pressable } from 'react-native'
 import {
   getDatabase,
   get,
@@ -13,8 +13,9 @@ import {
 const database = getDatabase()
 import { auth, db } from '../firebase-config'
 import colors from '../constants/colors'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
-export default function FriendsListScreen() {
+export default function FriendsListScreen(props) {
   const [userData, setUserData] = useState({})
 
   useEffect(() => {
@@ -24,22 +25,145 @@ export default function FriendsListScreen() {
     )
     onValue(dataAboutUser, (snapshot) => {
       setUserData(snapshot.val())
-      // console.log('===', snapshot.val().friends)
     })
   }, [])
 
   function renderFriends(friends) {
-    // console.log(friends)
+    let friendsArr = friends
+    delete friendsArr.separators
+
+    function renderFriend(friend) {
+      return (
+        <View
+          style={[
+            styles.friendBlock,
+            {
+              borderColor:
+                props.theme == 'white'
+                  ? colors.themeWhiteLine
+                  : colors.themeDarkBGPale,
+            },
+          ]}
+        >
+          <Text
+            style={[
+              styles.name,
+              { color: props.theme == 'white' ? colors.dark : colors.white },
+            ]}
+          >
+            {friend.item.name}
+          </Text>
+          <Text
+            style={[
+              styles.email,
+              {
+                color:
+                  props.theme == 'white'
+                    ? colors.themeWhiteComment
+                    : colors.themeDarkComment,
+              },
+            ]}
+          >
+            {friend.item.email}
+          </Text>
+          {/* <Text>{friend.item['chat-id']}</Text> */}
+
+          <View style={styles.buttonGroup}>
+            <Pressable
+              onPress={() => {
+                console.log('qwert')
+              }}
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed
+                    ? props.theme == 'white'
+                      ? colors.buttunActivePale
+                      : colors.themeDarkBG
+                    : props.theme == 'white'
+                    ? colors.themeWhiteBG
+                    : colors.themeDarkBGPale,
+                },
+                styles.touch,
+              ]}
+            >
+              {({ pressed }) => (
+                <Text
+                  style={[
+                    styles.buttonText,
+                    {
+                      color: pressed
+                        ? props.theme == 'white'
+                          ? colors.themeWhiteTextPale
+                          : colors.themeDarkText
+                        : props.theme == 'white'
+                        ? colors.themeWhiteComment
+                        : colors.themeDarkComment,
+                    },
+                  ]}
+                >
+                  Chat
+                </Text>
+              )}
+            </Pressable>
+            <Pressable
+              onPress={() => {
+                console.log('qwert')
+              }}
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed
+                    ? props.theme == 'white'
+                      ? colors.buttunActivePale
+                      : colors.themeDarkBG
+                    : props.theme == 'white'
+                    ? colors.themeWhiteBG
+                    : colors.themeDarkBGPale,
+                },
+                styles.touch,
+              ]}
+            >
+              {({ pressed }) => (
+                <Text
+                  style={[
+                    styles.buttonText,
+                    {
+                      color: pressed
+                        ? props.theme == 'white'
+                          ? colors.themeWhiteTextPale
+                          : colors.themeDarkText
+                        : props.theme == 'white'
+                        ? colors.themeWhiteComment
+                        : colors.themeDarkComment,
+                    },
+                  ]}
+                >
+                  Profile
+                </Text>
+              )}
+            </Pressable>
+          </View>
+        </View>
+      )
+    }
+
     return (
-      <View>
-        <Text>yujk</Text>
-      </View>
+      <FlatList
+        data={Object.values(friendsArr)[0].filter((i) => i != '-')}
+        renderItem={renderFriend}
+      />
     )
   }
 
   return (
-    <View>
-      <Text>Friends</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor:
+            props.theme == 'white' ? colors.themeWhiteBG : colors.themeDarkBG,
+        },
+      ]}
+    >
       <FlatList
         data={[Object.values({ ...userData.friends })]}
         renderItem={renderFriends}
@@ -47,3 +171,50 @@ export default function FriendsListScreen() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    flex: 1,
+  },
+  friendBlock: {
+    width: '90%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderRadius: 15,
+    marginTop: 5,
+    padding: 10,
+  },
+  name: { fontSize: 18 },
+  email: { color: '#666' },
+  buttonGroup: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    borderRadius: 5,
+    flex: 1,
+    padding: 5,
+    margin: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundColor: 'red',
+  },
+  touch: {
+    width: '45%',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    marginVertical: 2,
+    padding: 5,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#6D6C73',
+    marginLeft: 10,
+  },
+})
